74928a1a7474a36b848ba7b00fb9c638276dfcc8
DanRom521 <69353148+DanRom521@users.noreply.github.com>
2020-08-14 09:39:35 -0400

    Add files via upload

---
+++ Suma.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout << "Ingrese el primer numero: ";
+    cin >> a;
+    cout << "Ingrese el segundo numero: ";
+    cin >> b;
+    c = a+b;
+    cout << "El resultado es: " << c;
+    return 0;
+ }
